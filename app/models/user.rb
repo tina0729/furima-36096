1cb14b736ac5bba1a928a412058dead783df4b9b
tina0729 <riyo.forti@gmail.com>
2021-07-28 18:04:16 +0900

    新規登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true  
-  validates :first_name, presence: true 
-  validates :last_name, presence: true 
-  validates :first_name_kana, presence: true 
-  validates :last_name_kana, presence: true 
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} 
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} 
   validates :birth, presence: true
-  
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+
   has_many :products
   has_many :orders
 end
