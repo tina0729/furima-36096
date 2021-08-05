927a17f375dcc04bf56cee3a4a0d2d874fee0838
tina0729 <riyo.forti@gmail.com>
2021-08-05 11:25:12 +0900

    商品一覧表示機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  #def index
-    #@products = Product.all
-  #end
+  def index
+    @products = Product.all
+  end
 
   def new
     @product = Product.new
