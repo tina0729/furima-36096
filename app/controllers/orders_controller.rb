2e4821d4fb9206363d973978115040686aff71fb
tina0729 <riyo.forti@gmail.com>
2021-08-08 15:51:53 +0900

    購入者情報の保存とエラーメッセージの表示

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+    @product = Product.find(params[:product_id])
+  end
+  
+  def create
+    binding.pry
+    @product = Product.find(params[:product_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end    
+  end
+  
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone).merge(user_id: current_user.id, product_id: params[:product_id])
+  end
+end
