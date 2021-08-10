66d8096b9b948307577b33ed79b3b549bf4e2935
tina0729 <riyo.forti@gmail.com>
2021-08-10 10:53:15 +0900

    Rubocop2

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,12 +1,2 @@
 FactoryBot.define do
-  # factory :order_address do
-  #   postal_code                { 123 - 4567 }
-  #   prefecture_id              { 2 }
-  #   city                       {'東京都'}
-  #   address_line               {'番地1'}
-  #   phone                      { Faker::PhoneNumber }
-
-  #   association :order
-    
-  # end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,2 @@
 FactoryBot.define do
-  #factory :order_address do
-    #association :user
-    #association :product
-    
-  #end
 end
