a62787eaad6998f46739c12ca99161e0cca379e2
tina0729 <riyo.forti@gmail.com>
2021-08-10 10:52:19 +0900

    テストの修正とRubocopの実施

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
     @product = Product.find(params[:product_id])
   end
-  
+
   def create
     @product = Product.find(params[:product_id])
     @order_address = OrderAddress.new(order_params)
@@ -13,21 +13,23 @@ class OrdersController < ApplicationController
       redirect_to root_path
     else
       render :index
-    end    
+    end
   end
-  
+
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone).merge(
+      user_id: current_user.id, product_id: params[:product_id], token: params[:token]
+    )
   end
 
   def pay_product
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
-      Payjp::Charge.create(
-        amount: @product.price, 
-        card: order_params[:token],    
-        currency: 'jpy'                 
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,10 +2,9 @@ FactoryBot.define do
   factory :order_address do
     postal_code                { '123-4567' }
     prefecture_id              { 2 }
-    city                       {'東京都'}
-    address_line               {'番地1'}
-    phone                      { 12345678901 }
-    token                      {"tok_abcdefghijk00000000000000000"}  
-    
+    city                       { '東京都' }
+    address_line               { '番地1' }
+    phone                      { 12_345_678_901 }
+    token                      { 'tok_abcdefghijk00000000000000000' }
   end
 end
