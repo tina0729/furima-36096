d125aa0d78152895503043bf4573dbeb6a0368aa
tina0729 <riyo.forti@gmail.com>
2021-08-10 12:34:37 +0900

    レビュー依頼後修正1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :products do
-    resources :orders, only: [:index, :new, :create]
+    resources :orders, only: [:index, :create]
   end  
 end
